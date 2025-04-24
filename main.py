ff746717b8e7a4adc52ac9791eebe8349930a202
Paola Ghione <ghionep@gmail.com>
2025-04-25 00:51:51 +0200

    Update main script to utilize gpt-4o-mini model and enhance async functionality; add new dependencies
    - Updated main.py to use the gpt-4o-mini model for ChatOpenAI and improved async session handling.
    - Integrated HumanMessage for message formatting in agent invocation.
    - Added ipython and related dependencies in pyproject.toml and updated uv.lock accordingly.

--- main.py
+++ main.py
@@ -5,13 +5,15 @@ from dotenv import load_dotenv
 from mcp import ClientSession, StdioServerParameters
 from mcp.client.stdio import stdio_client
 
+from langchain_core.messages import HumanMessage
+
 from langchain_openai import ChatOpenAI
 from langchain_mcp_adapters.tools import load_mcp_tools
 from langgraph.prebuilt import create_react_agent
 
 load_dotenv()
 
-llm = ChatOpenAI()
+llm = ChatOpenAI(model="gpt-4o-mini")
 
 stdio_server_params = StdioServerParameters(
     command="python",
@@ -20,7 +22,22 @@ stdio_server_params = StdioServerParameters(
 
 
 async def main():
-    print("Hello from mcp-experiment!")
+    async with stdio_client(stdio_server_params) as (read, write):
+        async with ClientSession(read_stream=read, write_stream=write) as session:
+            await session.initialize()
+            print("Session initialized")
+            # List all tools available in the client
+            tools = await load_mcp_tools(session)
+            # print("Tools:", tools)
+
+            agent = create_react_agent(
+                llm,
+                tools,
+            )
+
+            # result = await agent.ainvoke({"messages": [HumanMessage(content="What is 2 + 2?")]}
+            result = await agent.ainvoke({"messages": [HumanMessage(content="What is 54 + 2 * 3?")]})
+            print(result["messages"][-1].content)
 
 
 if __name__ == "__main__":
